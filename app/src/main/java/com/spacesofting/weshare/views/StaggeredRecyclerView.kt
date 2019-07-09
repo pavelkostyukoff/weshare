cff5d04d784c8bef693b6c951468215929b0f971
Poul <Soprano61@yandex.ru>
2019-07-10 00:33:25 +0300

    set new view of feed fragment recycleView adapter

---
+++ app/src/main/java/com/spacesofting/weshare/views/StaggeredRecyclerView.kt
@@ -0,0 +1,34 @@
+package com.spacesofting.weshare.views
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.util.AttributeSet
+
+class StaggeredRecyclerView: RecyclerView {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    fun init() {
+        layoutManager = StaggeredGridLayoutManager(2, VERTICAL)
+        (layoutManager as StaggeredGridLayoutManager).gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+
+        if(getItemDecorationAt(0) == null) {
+            addItemDecoration(SpacesItemDecoration())
+        }
+    }
+
+    fun topPadding(paddingPixels: Int) {
+        setPadding(paddingLeft, paddingPixels, paddingRight, paddingBottom)
+    }
+}
